f0efa0f8a688bf547918aac1b04846cb28c95c67
Marius K <marius.into@gmail.com>
2019-11-16 22:56:26 +0200

    Add columns to gantt table.

--- OneNoteRibbonAddIn/Connect.cs
+++ OneNoteRibbonAddIn/Connect.cs
@@ -75,127 +75,165 @@ namespace OneNoteRibbonAddIn
             doc.Save("D:/one.xml");
 
             // OUTLINE //
-
-            var outline = new XElement(ns + "Outline",
-                new XElement(ns + "Position",
-                    new XAttribute("x", "36.0"),
-                    new XAttribute("y", "80.0")
-                ),
-                new XElement(ns + "Size",
-                    new XAttribute("width", "600.0"),
-                    new XAttribute("height", "100.0"),
-                    new XAttribute("isSetByUser", "true")
-                ),
-                new XElement(ns + "OEChildren",
-                    new XElement(ns + "OE",
-                        new XElement(ns + "T", new XCData("Startas: " + DateTime.Now.ToString("yyyy.MM.dd")))
+            if (doc.Descendants(ns + "Table").Count() == 0)
+            {
+                var outline = new XElement(ns + "Outline",
+                    new XElement(ns + "Position",
+                        new XAttribute("x", "36.0"),
+                        new XAttribute("y", "80.0")
                     ),
-                    new XElement(ns + "OE",
-                        new XElement(ns + "T", new XCData("Pabaiga: " + DateTime.Now.ToString("yyyy.MM.dd")))
+                    new XElement(ns + "Size",
+                        new XAttribute("width", "600.0"),
+                        new XAttribute("height", "100.0"),
+                        new XAttribute("isSetByUser", "true")
                     ),
-                    new XElement(ns + "OE",
-                        new XElement(ns + "T", new XCData(""))
-                    ),
-                    new XElement(ns + "OE",
-                        new XElement(ns + "Table",
-                            new XAttribute("bordersVisible", "true"),
-                            new XAttribute("hasHeaderRow", "true"),
+                    new XElement(ns + "OEChildren",
+                        new XElement(ns + "OE",
+                            new XElement(ns + "T", new XCData("Startas: " + DateTime.Now.ToString("yyyy.MM.dd")))
+                        ),
+                        new XElement(ns + "OE",
+                            new XElement(ns + "T", new XCData("Pabaiga: " + DateTime.Now.ToString("yyyy.MM.dd")))
+                        ),
+                        new XElement(ns + "OE",
+                            new XElement(ns + "T", new XCData(""))
+                        ),
+                        new XElement(ns + "OE",
+                            new XElement(ns + "Table",
+                                new XAttribute("bordersVisible", "true"),
+                                new XAttribute("hasHeaderRow", "true"),
 
-                            new XElement(ns + "Columns",
-                                new XElement(ns + "Column",
-                                    new XAttribute("index", "0"),
-                                    new XAttribute("width", "140.0"),
-                                    new XAttribute("isLocked", "true")
+                                new XElement(ns + "Columns",
+                                    new XElement(ns + "Column",
+                                        new XAttribute("index", "0"),
+                                        new XAttribute("width", "140.0"),
+                                        new XAttribute("isLocked", "true")
+                                    ),
+                                    new XElement(ns + "Column",
+                                        new XAttribute("index", "1"),
+                                        new XAttribute("width", "40.0"),
+                                        new XAttribute("isLocked", "true")
+                                    ),
+                                    new XElement(ns + "Column",
+                                        new XAttribute("index", "2"),
+                                        new XAttribute("width", "40.0"),
+                                        new XAttribute("isLocked", "true")
+                                    ),
+                                    new XElement(ns + "Column",
+                                        new XAttribute("index", "3"),
+                                        new XAttribute("width", "80.0"),
+                                        new XAttribute("isLocked", "true")
+                                    )
                                 ),
-                                new XElement(ns + "Column",
-                                    new XAttribute("index", "1"),
-                                    new XAttribute("width", "40.0"),
-                                    new XAttribute("isLocked", "true")
-                                ),
-                                new XElement(ns + "Column",
-                                    new XAttribute("index", "2"),
-                                    new XAttribute("width", "40.0"),
-                                    new XAttribute("isLocked", "true")
-                                ),
-                                new XElement(ns + "Column",
-                                    new XAttribute("index", "3"),
-                                    new XAttribute("width", "80.0"),
-                                    new XAttribute("isLocked", "true")
-                                )
-                            ),
 
-                            new XElement(ns + "Row",
-                                new XElement(ns + "Cell",
-                                    new XElement(ns + "OEChildren",
-                                        new XElement(ns + "OE",
-                                            new XAttribute("style", style),
-                                            new XAttribute("alignment", "center"),
-                                            new XElement(ns + "T", new XCData("UŽDUOTIS"))
+                                new XElement(ns + "Row",
+                                    new XElement(ns + "Cell",
+                                        new XElement(ns + "OEChildren",
+                                            new XElement(ns + "OE",
+                                                new XAttribute("style", style),
+                                                new XAttribute("alignment", "center"),
+                                                new XElement(ns + "T", new XCData("UŽDUOTIS"))
+                                            )
+                                        )
+                                    ),
+                                    new XElement(ns + "Cell",
+                                        new XElement(ns + "OEChildren",
+                                            new XElement(ns + "OE",
+                                                new XAttribute("style", style),
+                                                new XAttribute("alignment", "center"),
+                                                new XElement(ns + "T", new XCData("STARTAS"))
+                                            )
+                                        )
+                                    ),
+                                    new XElement(ns + "Cell",
+                                        new XElement(ns + "OEChildren",
+                                            new XElement(ns + "OE",
+                                                new XAttribute("style", style),
+                                                new XAttribute("alignment", "center"),
+                                                new XElement(ns + "T", new XCData("TRUKMĖ"))
+                                            )
+                                        )
+                                    ),
+                                    new XElement(ns + "Cell",
+                                        new XElement(ns + "OEChildren",
+                                            new XElement(ns + "OE",
+                                                new XAttribute("style", style),
+                                                new XAttribute("alignment", "center"),
+                                                new XElement(ns + "T", new XCData("KAS?"))
+                                            )
                                         )
                                     )
                                 ),
-                                new XElement(ns + "Cell",
-                                    new XElement(ns + "OEChildren",
-                                        new XElement(ns + "OE",
-                                            new XAttribute("style", style),
-                                            new XAttribute("alignment", "center"),
-                                            new XElement(ns + "T", new XCData("STARTAS"))
-                                        )
-                                    )
-                                ),
-                                new XElement(ns + "Cell",
-                                    new XElement(ns + "OEChildren",
-                                        new XElement(ns + "OE",
-                                            new XAttribute("style", style),
-                                            new XAttribute("alignment", "center"),
-                                            new XElement(ns + "T", new XCData("TRUKMĖ"))
-                                        )
-                                    )
-                                ),
-                                new XElement(ns + "Cell",
-                                    new XElement(ns + "OEChildren",
-                                        new XElement(ns + "OE",
-                                            new XAttribute("style", style),
-                                            new XAttribute("alignment", "center"),
-                                            new XElement(ns + "T", new XCData("KAS?"))
-                                        )
-                                    )
-                                )
-                            ),
 
-                            new XElement(ns + "Row",
-                                new XElement(ns + "Cell",
-                                    new XElement(ns + "OEChildren",
-                                        new XElement(ns + "OE",
-                                            new XElement(ns + "T", new XCData("Uzregistruoti MS"))
+                                new XElement(ns + "Row",
+                                    new XElement(ns + "Cell",
+                                        new XElement(ns + "OEChildren",
+                                            new XElement(ns + "OE",
+                                                new XElement(ns + "T", new XCData("Uzregistruoti MS"))
+                                            )
                                         )
-                                    )
-                                ),
-                                new XElement(ns + "Cell",
-                                    new XElement(ns + "OEChildren",
-                                        new XElement(ns + "OE",
-                                            new XElement(ns + "T", new XCData("1"))
+                                    ),
+                                    new XElement(ns + "Cell",
+                                        new XElement(ns + "OEChildren",
+                                            new XElement(ns + "OE",
+                                                new XElement(ns + "T", new XCData("1"))
+                                            )
+                                        )
+                                    ),
+                                    new XElement(ns + "Cell",
+                                        new XElement(ns + "OEChildren",
+                                            new XElement(ns + "OE",
+                                                new XElement(ns + "T", new XCData("1"))
+                                            )
+                                        )
+                                    ),
+                                    new XElement(ns + "Cell",
+                                        new XElement(ns + "OEChildren",
+                                            new XElement(ns + "OE",
+                                                new XElement(ns + "T", new XCData(""))
+                                            )
                                         )
                                     )
                                 )
                             )
                         )
                     )
-                )
-            );
+                );
 
-            var page = doc.Descendants(ns + "Page").First();
-            page.Add(outline);
+                var page = doc.Descendants(ns + "Page").First();
+                page.Add(outline);
 
-            // GANTT COLUMNS //
-            //addColumn();
+            }
             
             int cols = doc.Descendants(ns + "Table").First().Descendants(ns+"Column").Count();
+            int _cols = cols;
+
+            int startColumn = 2;
+            int durationColumn = 3;
+            int col = 0;
+            int start = 0;
+            int duration = 0;
+            int maxPeriod = 0;
 
             var items = doc.Descendants(ns + "Table").First().Descendants(ns + "Cell");
-            foreach (var i in items)
+            foreach (var item in items)
             {
-               // MessageBox.Show(i.Value);
+                col++;
+                if (col == startColumn) Int32.TryParse(item.Value, out start);
+                if (col == durationColumn & start > 0 & Int32.TryParse(item.Value, out duration))
+                {
+                    if (start + duration - 1 > maxPeriod) maxPeriod = start + duration - 1;
+                }
+
+                if (col == _cols) col = 0;
+            }
+            
+            addColumns(maxPeriod + 4 - cols);
+            cols = doc.Descendants(ns + "Table").First().Descendants(ns + "Column").Count();
+
+            var cells = doc.Descendants(ns + "Table").First().Descendants(ns + "Cell");
+            foreach (var cell in cells)
+            {
+
             }
 
             doc.Save("D:/doc.xml");
@@ -203,32 +241,41 @@ namespace OneNoteRibbonAddIn
 
         }
 
-        public void addColumn()
+        public void addColumns(int cols)
         {
-            int col = doc.Descendants(ns + "Table").First().Descendants(ns + "Column").Count()-3;
-            var columns = doc.Descendants(ns + "Table").First().Descendants(ns + "Columns").First();
-            var column = new XElement(ns + "Column",
-                new XAttribute("index", "4"),
-                new XAttribute("width", "20.0"),
-                new XAttribute("isLocked", "true")
-            );
-            columns.Add(column);
+            for (var c = 0; c < cols; c++)
+            {
+                int col = doc.Descendants(ns + "Table").First().Descendants(ns + "Column").Count() - 3;
+                var columns = doc.Descendants(ns + "Table").First().Descendants(ns + "Columns").First();
+                var column = new XElement(ns + "Column",
+                    new XAttribute("index", "4"),
+                    new XAttribute("width", "20.0"),
+                    new XAttribute("isLocked", "true")
+                );
+                columns.Add(column);
 
-            var row = doc.Descendants(ns + "Table").First().Descendants(ns + "Row").First();
-            var cell = new XElement(ns + "Cell",
-                new XAttribute("shadingColor", "#FAFAFA"),
-                new XElement(ns + "OEChildren",
-                    new XElement(ns + "OE",
-                        new XAttribute("style", style),
-                        new XAttribute("alignment", "center"),
-                        new XElement(ns + "Meta",
-                            new XAttribute("name", "GANTT")
-                        ),
-                        new XElement(ns + "T", new XCData(col.ToString()))
-                    )
-                )
-            );
-            row.Add(cell);
+                var rows = doc.Descendants(ns + "Table").First().Descendants(ns + "Row");
+                var r = 0;
+                var txt = col.ToString();
+                
+                foreach (var row in rows)
+                {
+                    r++;
+                    if (r > 1) txt = "";
+                    var cell = new XElement(ns + "Cell",
+                        new XElement(ns + "OEChildren",
+                            new XElement(ns + "OE",
+                                new XAttribute("style", style),
+                                new XAttribute("alignment", "center"),
+                                new XElement(ns + "T", new XCData(txt))
+                            )
+                        )
+                    );
+                    if (col % 2 > 0) cell.Add(new XAttribute("shadingColor", "#FAFAFA"));
+                    row.Add(cell);
+                }
+
+            }
         }
 
 
